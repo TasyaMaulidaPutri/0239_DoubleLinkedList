d27dea91fc52f686f5297f15ec00f300f1796127
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-05-13 11:31:20 +0700

    method untuk mengecek apakah list kosong

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -151,7 +151,13 @@ void deleteNode()
     cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m " << endl;
 }
 
+//method untuk mengecek apakah list kosong
+bool listEmpty()
+{
+    return (START == NULL);
 
-    
+}
+
+
 
         
