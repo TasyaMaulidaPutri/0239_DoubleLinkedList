bb762be8727388fb6d890ae7551a381a5bbdf112
TasyaMaulidaPutri <tasyamaulidaputri06@gamil.com>
2025-05-13 10:13:04 +0700

    deklarasi noMhs dan name untuk menampung data

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -4,5 +4,9 @@ using namespace std;
 // membuat struktur untuk object double LinkedList
 struct Node
 {
+    // deklarasi noMhs dan name untuk menampung data
+    int noMhs;
+    string name;
+    
 
 };
